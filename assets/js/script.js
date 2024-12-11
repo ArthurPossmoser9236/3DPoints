1641ffd597b6998659a48a3ddd308376da9a9c65
Arthur Possmoser <162332669+ArthurPossmoser9236@users.noreply.github.com>
2024-12-11 14:37:01 -0300

    teste

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+// redirecionar para Login e Cadastro
+function redirectToLogin() {
+    window.location.href = 'login.html';
+}
+
+function redirectToRegister() {
+    window.location.href = 'register.html';
+}
